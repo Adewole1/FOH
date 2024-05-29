2d3ec76ada86a93535db3f4f5c8303c8d436da9d
Michael Adedayo <madedayo@gmail.com>
2024-05-29 15:19:11 +0100

    Updated background images location

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -11,10 +12,10 @@ export default {
         green: "#357266",
       },
       backgroundImage: {
-        doctorsBG: `url('./src/assets/images/doctors.png')`,
-        panelBG: `url('./src/assets/images/panel.png')`,
-        expertsBG: `url('./src/assets/images/expert.png')`,
-        speakersBG: `url('./src/assets/images/speaker.jpg')`,
+        doctorsBG: `url('/images/doctors.png')`,
+        panelBG: `url('/images/panel.png')`,
+        expertsBG: `url('/images/expert.png')`,
+        speakersBG: `url('/images/speaker.jpg')`,
       },
     },
   },
